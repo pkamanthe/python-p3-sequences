6daca9f6820b1596d66f33b2d3f576bbaadc9477
pkamanthe <patriciamumbua97@gmail.com>
2024-12-04 08:36:07 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print("[]")  
+        return
+    elif n == 1:
+        print("[0]")  
+        return
+
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    
+    print(fib_sequence)
